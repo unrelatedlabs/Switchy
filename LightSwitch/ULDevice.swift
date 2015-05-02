8b4052c302e89b6a6c187c6d5f3d19e6751f90dd
Peter Kuhar <peter@azumio.com>
2015-05-02 09:20:59 -0700

    first commit

---
+++ LightSwitch/ULDevice.swift
@@ -0,0 +1,13 @@
+//
+//  ULDevice.swift
+//  LightSwitch
+//
+//  Created by Peter Kuhar Jagodnik on 5/2/15.
+//  Copyright (c) 2015 Peter Kuhar. All rights reserved.
+//
+
+import UIKit
+
+class ULDevice: NSObject {
+   
+}
---
+++ LightSwitch/ULDeviceController.swift
@@ -0,0 +1,13 @@
+//
+//  ULDeviceController.swift
+//  LightSwitch
+//
+//  Created by Peter Kuhar Jagodnik on 5/2/15.
+//  Copyright (c) 2015 Peter Kuhar. All rights reserved.
+//
+
+import UIKit
+
+class ULDeviceController: NSObject {
+   
+}
